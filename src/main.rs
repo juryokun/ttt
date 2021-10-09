66f82aaed4066a30fc2828ef679af522b93b3fc9
juryokun <capeta.kozo@gmail.com>
2021-10-09 17:47:51 +0900

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
 }
+// curl -X POST 'https://api.notion.com/v1/databases/${database_id}/query' \ 812ms  2021年10月09日 17時27分57秒
+//            -H 'Authorization: Bearer '"${api_key}"'' \
+//            -H 'Notion-Version: 2021-08-16' \
+//            -H "Content-Type: application/json" \
+//            --data '{
+//            "filter": {
+//                "and": [
+//                    {
+//                        "property": "Today flag",
+//                        "checkbox": {
+//                          "equals": true
+//                        }
+//                    },
+//                    {
+//                        "property": "Done flag",
+//                        "checkbox": {
+//                          "equals": false
+//                        }
+//                    }
+//                ]
+//            }
+//        }'
