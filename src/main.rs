5214c126e13384d6d7914c5883257153d9872a3f
juryokun <capeta.kozo@gmail.com>
2021-10-10 14:13:27 +0900

    add function of loading config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+use std::sync::Mutex;
+extern crate dirs;
+
+static CONFIG_DATA: Lazy<Mutex<Config>> = Lazy::new(|| {
+    let mut file_name = get_settings_dir();
+    file_name.push("settings.json");
+    let reader = BufReader::new(File::open(file_name).unwrap());
+    let config: Config = serde_json::from_reader(reader).unwrap();
+    Mutex::new(config)
+});
+
+#[cfg(not(test))]
+fn get_settings_dir() -> PathBuf {
+    let mut config_dir = dirs::config_dir().unwrap();
+    config_dir.push("ttt");
+    config_dir
+}
+#[cfg(test)]
+fn get_settings_dir() -> PathBuf {
+    PathBuf::from("rsc/")
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -23,3 +50,19 @@ fn main() {
 //                ]
 //            }
 //        }'
+fn sync_notion() {
+    unimplemented!();
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    database_id: String,
+    api_key: String,
+}
+
+#[test]
+fn test_load_settings() {
+    let config = CONFIG_DATA.lock().unwrap();
+    println!("{}", config.api_key);
+    println!("{}", config.database_id);
+}
